0d4195c8f39c48af3c56da157df3a0ffe45bc6b9
ChukharevIlya <chukharevowen@yandex.ru>
2022-09-16 20:29:41 +0300

    test added

---
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -0,0 +1,54 @@
+package ru.netology.delivery.data;
+
+import com.github.javafaker.Faker;
+import lombok.Data;
+import lombok.Value;
+import lombok.experimental.UtilityClass;
+import lombok.val;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+
+    private DataGenerator() {
+    }
+
+    public static Faker faker = new Faker(new Locale("ru"));
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity(String locale) {
+        return faker.address().city();
+    }
+
+    public static String generateName(String locale) {
+        return faker.name().fullName();
+    }
+
+    public static String generatePhone(String locale) {
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            UserInfo user = new UserInfo(generateCity("ru"), generateName("ru"), generatePhone("ru"));
+            return user;
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
---
+++ src/test/java/ru/netology/delivery/test/AppCardDeliveryTest.java
@@ -0,0 +1,48 @@
+package ru.netology.delivery.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.delivery.data.DataGenerator;
+
+import java.security.Key;
+import java.time.Duration;
+import java.util.Locale;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+public class AppCardDeliveryTest {
+
+    @BeforeEach
+    void setup() {
+        Configuration.holdBrowserOpen = false;
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldSuccessfulPlanAndReplanMeeting() throws InterruptedException{
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $x("//*[@data-test-id=\"city\"] //*[@placeholder=\"Город\"]").setValue(validUser.getCity());
+        $x("//*[@data-test-id=\"date\"] //*[@placeholder=\"Дата встречи\"]").setValue(firstMeetingDate);
+        $x("//*[@data-test-id=\"name\"] //*[@name=\"name\"]").setValue(validUser.getName());
+        $x("//*[@data-test-id=\"phone\"] //*[@name=\"phone\"]").setValue(validUser.getPhone());
+        $x("//*[@data-test-id=\"agreement\"]").click();
+        $x("//*[@type=\"button\"]//span[contains(text(),\"Запланировать\")]").click();
+        $x("//*[@data-test-id=\"success-notification\"]").should(Condition.visible, Duration.ofSeconds(15));
+        $x("//*[@data-test-id=\"date\"] //*[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(Keys.DELETE);
+        $x("//*[@data-test-id=\"date\"] //*[@placeholder=\"Дата встречи\"]").setValue(secondMeetingDate);
+        $x("//*[@type=\"button\"]//span[contains(text(),\"Запланировать\")]").click();
+        $x("//*[@data-test-id=\"replan-notification\"]//span[text()=\"Перепланировать\"]").click();
+        $x("//*[@data-test-id=\"success-notification\"]//div[contains(text(),\"Встреча успешно запланирована на \")]").shouldBe(Condition.visible).shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate));
+    }
+}
