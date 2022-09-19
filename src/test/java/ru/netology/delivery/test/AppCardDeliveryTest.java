810207e0fcf2ff12a6f794ee78ca542b2560cf43
ChukharevIlya <chukharevowen@yandex.ru>
2022-09-19 13:02:05 +0300

    test changed

--- src/test/java/ru/netology/delivery/test/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/AppCardDeliveryTest.java
@@ -20,7 +20,7 @@ public class AppCardDeliveryTest {
 
     @BeforeEach
     void setup() {
-        Configuration.holdBrowserOpen = false;
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
     }
 
@@ -33,12 +33,13 @@ public class AppCardDeliveryTest {
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
         $x("//*[@data-test-id=\"city\"] //*[@placeholder=\"Город\"]").setValue(validUser.getCity());
+        $x("//*[@data-test-id=\"date\"] //*[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(Keys.DELETE);
         $x("//*[@data-test-id=\"date\"] //*[@placeholder=\"Дата встречи\"]").setValue(firstMeetingDate);
         $x("//*[@data-test-id=\"name\"] //*[@name=\"name\"]").setValue(validUser.getName());
         $x("//*[@data-test-id=\"phone\"] //*[@name=\"phone\"]").setValue(validUser.getPhone());
         $x("//*[@data-test-id=\"agreement\"]").click();
         $x("//*[@type=\"button\"]//span[contains(text(),\"Запланировать\")]").click();
-        $x("//*[@data-test-id=\"success-notification\"]").should(Condition.visible, Duration.ofSeconds(15));
+        $x("//*[@data-test-id=\"success-notification\"]//div[contains(text(),\"Встреча успешно запланирована на \")]").should(Condition.visible, Duration.ofSeconds(15)).shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate));
         $x("//*[@data-test-id=\"date\"] //*[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(Keys.DELETE);
         $x("//*[@data-test-id=\"date\"] //*[@placeholder=\"Дата встречи\"]").setValue(secondMeetingDate);
         $x("//*[@type=\"button\"]//span[contains(text(),\"Запланировать\")]").click();
